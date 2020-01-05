055d90f6cc02be750c1b6e20a700ee21eb864c52
kakoi-to-lalka <kshevnin013@gmail.com>
2020-01-05 11:56:39 +0300

    дописал

--- vk-friends-online.py
+++ vk-friends-online.py
@@ -8,4 +8,5 @@ f.close()
 r=requests.get('https://api.vk.com/method/friends.getOnline?v=5.52&access_token=' + token)
 friends = json.load(r.content)
 
-print(friends)
+online - len(friends['response'])
+print('Всего друзей онлайн:', online)
